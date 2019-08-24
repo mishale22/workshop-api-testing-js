76f835ac643d3ad2898c6a3dab2aa036a2d8a7bc
mishale22 <sami_na11@hotmail.com>
2019-08-24 11:09:27 -0500

    requestBody for query

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -13,16 +13,16 @@ describe('First Api Tests', () => {
       });
 
     it('Consume GET Service with query parameters', async () => {
-        const query = {
+        const requestBody = {
           name: 'John',
           age: '31',
           city: 'New York'
         };
       
-        const response = await agent.get('https://httpbin.org/get').query(query);
+        const response = await agent.get('https://httpbin.org/get').query(requestBody);
       
         expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(query);
+        expect(response.body.args).to.eql(requestBody);
       });
       
     it('Consume DELETE service', async () => {
@@ -35,27 +35,27 @@ describe('First Api Tests', () => {
 
     it('Consume PUT service', async () => {
 
-        const body ={
+        const requestBody = {
             name: "John",
             city: "LA"
         }
 
-        const response = await agent.put('https://httpbin.org/put').query(body);
+        const response = await agent.put('https://httpbin.org/put').query(requestBody);
    
         expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(body);
+        expect(response.body.args).to.eql(requestBody);
     });
 
     it('Consume PATCH service', async () => {
 
-        const body ={
+        const requestBody = {
             name: "John"
         }
 
-        const response = await agent.patch('https://httpbin.org/patch').query(body);
+        const response = await agent.patch('https://httpbin.org/patch').query(requestBody);
         
         expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(body);
+        expect(response.body.args).to.eql(requestBody);
     });
 
     it('Consume HEAD service', async () => {
